cbda0c2c9b54345e7b662f173437a7ae5c553bcb
tony <chcjswo@gmail.com>
2022-05-10 23:41:05 +0900

    JpaCursorItemReader

--- src/main/java/me/mocadev/springbatch/part3/ItemReaderConfiguration.java
+++ src/main/java/me/mocadev/springbatch/part3/ItemReaderConfiguration.java
@@ -3,6 +3,7 @@ package me.mocadev.springbatch.part3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +14,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -37,6 +40,7 @@ public class ItemReaderConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 	private final DataSource dataSource;
+	private final EntityManagerFactory entityManagerFactory;
 
 	@Bean
 	public Job itemReaderJob() throws Exception {
@@ -45,6 +49,7 @@ public class ItemReaderConfiguration {
 			.start(this.customItemReaderStep())
 			.next(this.csvFileStep())
 			.next(this.jdbcStep())
+			.next(this.jpaStep())
 			.build();
 	}
 
@@ -64,6 +69,7 @@ public class ItemReaderConfiguration {
 			.writer(itemWriter())
 			.build();
 	}
+
 	@Bean
 	public Step jdbcStep() throws Exception {
 		return stepBuilderFactory.get("jdbcStep")
@@ -73,16 +79,35 @@ public class ItemReaderConfiguration {
 			.build();
 	}
 
+	@Bean
+	public Step jpaStep() throws Exception {
+		return stepBuilderFactory.get("jpaStep")
+			.<Person, Person>chunk(10)
+			.reader(this.jpaCursorItemReader())
+			.writer(itemWriter())
+			.build();
+	}
+
+	private JpaCursorItemReader<Person> jpaCursorItemReader() throws Exception {
+		final JpaCursorItemReader<Person> itemReader = new JpaCursorItemReaderBuilder<Person>()
+			.name("jpaCursorItemReader")
+			.entityManagerFactory(entityManagerFactory)
+			.queryString("select p from person p")
+			.build();
+		itemReader.afterPropertiesSet();
+		return itemReader;
+	}
+
 	private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
-		final JdbcCursorItemReader<Person> jdbcCursorItemReader = new JdbcCursorItemReaderBuilder<Person>()
+		final JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
 			.name("jdbcCursorItemReader")
 			.dataSource(dataSource)
 			.sql("select id, name, age, address from person")
 			.rowMapper(((rs, rowNum) -> new Person(rs.getInt(1), rs.getString(2), rs.getString(3),
 				rs.getString(4))))
 			.build();
-		jdbcCursorItemReader.afterPropertiesSet();
-		return jdbcCursorItemReader;
+		itemReader.afterPropertiesSet();
+		return itemReader;
 	}
 
 	private ItemWriter<Person> itemWriter() {
--- src/main/java/me/mocadev/springbatch/part3/Person.java
+++ src/main/java/me/mocadev/springbatch/part3/Person.java
@@ -1,7 +1,11 @@
 package me.mocadev.springbatch.part3;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * @author chcjswo
@@ -10,13 +14,26 @@ import lombok.Getter;
  * @github https://github.com/chcjswo
  * @since 2022-05-04
  **/
+@Entity
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String age;
 	private String address;
 
+	public Person(String name, String age, String address) {
+		this(0, name, age, address);
+	}
+
+	public Person(int id, String name, String age, String address) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
 }
